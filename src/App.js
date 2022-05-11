f18e6febb30da82d8135bc7b38490aff67b2f364
Fazelpejmanfar <fazelpejmanfar@gmail.com>
2022-05-11 06:05:13 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { InputNumber } from 'rsuite';
+import "rsuite/dist/rsuite.min.css"
+import { useState } from 'react';
 
 function App() {
+const [WEI, setWEI] = useState(1000);
+const [ETH, setETH] = useState(0.01);
+console.log('eth', ETH);
+console.log('wei', WEI);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='inputs'>
+        <div className='weitoeth'>
+          <h3> WEI to ETH</h3>
+      <InputNumber style={{width: 350}} min={1000000000000} prefix="WEI"
+      onChange={ e => {setETH(e / 1e18)}}
+      />
+
+      <InputNumber style={{width: 350}} min={0} readOnly postfix="ETH" value={ETH}
+      />
+       </div>
+       <div className='ethtowei'>
+       <h3> ETH to WEI</h3>
+      <InputNumber style={{width: 350}} min={0} max={999} prefix="ETH"
+       onChange={ e => {setWEI(e * 1000000000000000000)}}
+      />
+
+      <InputNumber style={{width: 350}} min={0} readOnly postfix="WEI" value={WEI}
+      />
+      </div>
+
+      </div>
       </header>
     </div>
   );
